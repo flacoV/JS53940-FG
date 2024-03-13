b8bf67d3daa099b8b8a894c6150bf337bd608271
devflaco <npressure00@gmail.com>
2024-03-13 10:14:27 -0300

    arrays añadidos

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
 
+
 //Funcion para guardar
 function cotizador(ancho, largo, tela) {
     let manoDeObra = 10000; 
     let precio;
+    //Array de precio segun el largo de la tela
+    const precioLargo = [0, 3000, 4150, 6100, 9000, 15000];
 
     
     //Utilizo condicional para aplicar el descuento y para los precios segun el largo de la tela.
@@ -14,44 +17,45 @@ function cotizador(ancho, largo, tela) {
 
 
     if (largo <= 100) {
-        precio += 3000;
+        precio += precioLargo[1];
     } else if (largo <= 150) {
-        precio += 4150;
+        precio += precioLargo[2];
     } else if (largo <= 220) {
-        precio += 6100;
+        precio += precioLargo[3];
     } else if (largo <= 300) {
-        precio += 9000;
+        precio += precioLargo[4];
     } else {
-        precio += 15000;
+        precio += precioLargo[5];
     }
 
+    //Creo un array e ingreso las telas y con sus respectivos nombres y precios
+    const telas = [
+        {nombre: "screen", precio: 57000},
+        {nombre: "blackout", precio: 45000},
+        {nombre: "lino", precio: 20000},
+    ]
 
-    //Utilizo switch para aplicar las distintas opciones de tela y sus precios.
-    switch (tela.toLocaleLowerCase()) {
-        case "blackout":
-            precio += 45000;
-            break;
-        case "screen":
-            precio += 57000;
-            break;
-        case "lino":
-            precio += 20000;
-            break;
-        
-        default:
-            console.log("No comercializamos esa tela");
-            break;
+    // Busco dentro del array de telas si lo ingresado por el usuario es correcto.
+    const tipoDeTela = telas.find(
+        (t) => t.nombre.toLowerCase() ===  tela.toLowerCase()
+    );
+
+    //Utilizo un condicional y le digo al programa basicamente que si el find encontro un objeto dentro
+    //del array(telas) que fue ingresado por el cliente que le sume el precio de la tela a lo ya
+     // calculado segun las medidas ingresadas anteriormente.
+    if (tipoDeTela) {
+        precio += tipoDeTela.precio;
+    } else {
+        alert("Lo siento!, Estas ingresando una tela no valida o no disponible en este momento.")
     }
 
     return precio;
 
-    
 
 }
 
 
-
-let welcome = alert('Somos RollTech, presupueste su cortina roller en menos de 5 minutos!!!');
+alert('Somos RollTech, presupueste su cortina roller en menos de 5 minutos!!!');
 let userName = prompt('Ingrese su nombre para una experiencia personalizada');
 let discAlert = alert(`Bienvenido ${userName}, recuerda que hay un descuento del 35% en cortinas de 150cm o menos de ancho`);
 let ancho = parseInt(prompt('Ingrese el ancho de su cortinar en centimetros (cm)'));
@@ -69,3 +73,7 @@ do {
         console.log(`Se ha aplicado el descuento del 35% correctamente al presupuesto de ${userName}`);
     }
 } while (false);
+
+if (ancho < 50, largo < 100) {
+    alert("Porfavor ingrese una medida valida");
+}
