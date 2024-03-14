aa157f55b8f41dc68c73558d86a40cc49f7b23b2
devflaco <npressure00@gmail.com>
2024-03-14 20:39:16 -0300

    validaciones de prompts agregadas

--- js/app.js
+++ js/app.js
@@ -1,4 +1,12 @@
 
+//Declaro la funcion letras para validar si el string contiene solo letras y espacios en blanco, utilizo la expresion (/^[A-Za-z\s]+$/ y /^[0-9]+$/) para validar las cadenas de texto, seguida de un .test que aplica a texto dando true o false segun respete el patron anteriormente especificado y repito el mismo proceso pero no la funcion numeros
+function letras(texto) {
+    return /^[A-Za-z\s]+$/.test(texto);
+}
+
+function numeros(texto) {
+    return /^[0-9]+$/.test(texto);
+}
 
 //Funcion para guardar
 function cotizador(ancho, largo, tela) {
@@ -56,14 +64,53 @@ function cotizador(ancho, largo, tela) {
 
 
 alert('Somos RollTech, presupueste su cortina roller en menos de 5 minutos!!!');
-let userName = prompt('Ingrese su nombre para una experiencia personalizada');
-let discAlert = alert(`Bienvenido ${userName}, recuerda que hay un descuento del 35% en cortinas de 150cm o menos de ancho`);
-let ancho = parseInt(prompt('Ingrese el ancho de su cortinar en centimetros (cm)'));
-let largo = parseInt(prompt('Ingrese el largo de la tela en centimetros (cm)'));
-let tela = prompt('Ingrese el nombre de la tela - (BlackOut - Screen - Lino)');
+
+
+//Declaro la variable userName pero no la defino, agrego el prompt en un bucle (do-while) llamando a la funcion letras
+let userName;
+do {
+    userName = prompt('Ingrese su nombre para una experiencia personalizada');
+} while (!letras(userName));
+
+
+alert(`Bienvenido ${userName}, recuerda que hay un descuento del 35% en cortinas de 150cm o menos de ancho`);
+
+
+//Utilizo el mismo tipo de bucle que para userName solo que en el while agrego un .toString() para que el codigo lo lea como un numbre y no string tambien sume un el opreador avanzado para tambien condicionar el minimo de cada medida y asi acorte codigo.
+let ancho;
+do {
+    ancho = parseInt(prompt('Ingrese el ancho de su cortinar en centimetros (cm)'));
+    if ( ancho < 50 ) {
+        alert('El ancho debe ser de al menos 50 cm. Porfavor, ingrese un valor valido');
+    }
+} while (!numeros(ancho.toString()) || ancho < 50);
+
+let largo;
+do {
+    largo = parseInt(prompt('Ingrese el largo de la tela en centimetros (cm)'));
+    if ( largo < 100) {
+        alert('El largo debe ser de al menos 100cm. Porfavor ingrese un valor valido');
+    }
+} while (!numeros(largo.toString()) || largo < 100);
+
+
+//Declaro la variable tela (undefined) y la variable telStock le asigno un valor de false y dentro del ciclo aplico la misma logica utilizada para el ancho y largo solo que lo condiciono con los nombres de las telas, para asi ejecutar el bucle siempre y cuando sea true.
+let tela;
+let telaStock = false;
+do {
+    tela = prompt('Ingrese el nombre de la tela - (BlackOut - Screen - Lino)');
+
+    if (tela.toLocaleLowerCase() === 'blackout' || tela.toLocaleLowerCase() === 'screen' || tela.toLocaleLowerCase() === 'lino') {
+        telaStock = true;
+    } else {
+        alert('Por favor, ingrese una tela válida (BlackOut, Screen, Lino).');
+    }
+} while (!telaStock);
+
 let cotizacion = cotizador(ancho, largo, tela);
 
 console.log(`Cotizacion para ${userName}, medidas de la cortina (${ancho}cm x ${largo}cm) en tela ${tela} valor total= $${cotizacion}`);
+
 alert(`La cotizacion para ${userName} es $${cotizacion}. ¡Gracias por utilizar nuestros servicios!`);
 
 
@@ -73,7 +120,3 @@ do {
         console.log(`Se ha aplicado el descuento del 35% correctamente al presupuesto de ${userName}`);
     }
 } while (false);
-
-if (ancho < 50, largo < 100) {
-    alert("Porfavor ingrese una medida valida");
-}
